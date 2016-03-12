8e89291d3d82840a4f71a304c40576837e88d85b
张新 <zhangxin@zhangxindemacbook-pro.local>
2016-03-12 20:01:19 +0800

    Initial Commit

---
+++ qiniuUploadImageText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qiniuUploadImageText
+//
+//  Created by 张新 on 16/3/12.
+//  Copyright © 2016年 zhangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qiniuUploadImageText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qiniuUploadImageText
+//
+//  Created by 张新 on 16/3/12.
+//  Copyright © 2016年 zhangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
