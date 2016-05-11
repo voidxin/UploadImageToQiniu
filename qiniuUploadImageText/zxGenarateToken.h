bdfbd624f01ec33004e29d388183c647c6c89558
张新 <zhangxin@zhangxindeMacBook-Pro.local>
2016-05-12 00:50:41 +0800

    七牛上传

---
+++ qiniuUploadImageText/zxGenarateToken.h
@@ -0,0 +1,13 @@
+//
+//  zxGenarateToken.h
+//  qiniuUploadImageText
+//
+//  Created by 张新 on 16/3/12.
+//  Copyright © 2016年 zhangxin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface zxGenarateToken : NSObject
+
+@end
---
+++ qiniuUploadImageText/zxGenarateToken.m
@@ -0,0 +1,13 @@
+//
+//  zxGenarateToken.m
+//  qiniuUploadImageText
+//
+//  Created by 张新 on 16/3/12.
+//  Copyright © 2016年 zhangxin. All rights reserved.
+//
+
+#import "zxGenarateToken.h"
+
+@implementation zxGenarateToken
+
+@end
